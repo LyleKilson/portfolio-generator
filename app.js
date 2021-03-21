710eac1530454af313697afe628987446199703a
Lyle Kilson <lylekilsongithub@gmail.com>
2021-03-21 13:32:46 -0600

    installed inquirer package

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generatePage = require('./src/page-template.js')
+inquirer
+  .prompt([
+    {
+      message: "What is your name?",
+      type: "input",
+      name: "name"
+    },
+  ])
 
-const profileDataArgs = process.argv.slice(2);
+  .then((answers) => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require("fs");
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+// const generatePage = require('./src/page-template.js')
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
